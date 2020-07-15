01b97e81bc9bf63a76301f1484b0425f4c3179f8
Alistair Burrowes <alistair.burrowes@gmail.com>
2020-07-15 10:43:29 +1000

    Update to fused effects 1.1

--- src/EmitErrorsC.hs
+++ src/EmitErrorsC.hs
@@ -5,14 +5,16 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE GADTs #-}
 module EmitErrorsC (EmitErrorsC(..)) where
 
-import Control.Algebra ((:+:) (..), Algebra (..), handleCoercible)
+import Control.Algebra ((:+:) (..), Algebra (..))
 import Control.Carrier.Error.Either
 import Control.Monad.IO.Class (MonadIO)
 import GHC.Base (Type)
 import AppEventEmit
 import Control.Monad.Except (ExceptT(..))
+import Control.Monad.Trans.Except (catchE)
 
 newtype EmitErrorsC (e :: Type) m a = EmitErrorsC {runEmitErrorsC :: ErrorC e m a}
   deriving (Functor, Applicative, Monad, MonadIO)
@@ -20,22 +22,19 @@ newtype EmitErrorsC (e :: Type) m a = EmitErrorsC {runEmitErrorsC :: ErrorC e m 
 hoistEither :: Applicative m => Either e a -> ExceptT e m a
 hoistEither e = ExceptT (pure e)
 
-instance (Effect sig, Algebra sig m, Has AppEventEmit sig m) => Algebra (Error e :+: sig) (EmitErrorsC e m) where
-  alg (L (L (Throw e))) = do
-    emitAppEvent "boom"
-    EmitErrorsC $ ErrorC $ hoistEither (Left e)
-  alg (L (R (Catch m h k))) =
-    EmitErrorsC $
-      ErrorC $
-        ExceptT $
-          runCatch m >>= \case
-            Left err -> runCatch (h err >>= k)
-            Right result -> runCatch (k result)
-
-  alg (R other) = EmitErrorsC (alg (R (handleCoercible other)))
+instance (Algebra sig m, Has AppEventEmit sig m) => Algebra (Error e :+: sig) (EmitErrorsC e m) where
+  alg hdl sig ctx = case sig of
+    (L (L (Throw e))) -> do
+      emitAppEvent "boom"
+      EmitErrorsC $ ErrorC $ hoistEither (Left e)
+    (L (R (Catch m h))) ->
+      EmitErrorsC $
+        ErrorC $
+          catchE (runCatch $ hdl (m <$ ctx)) (runCatch . hdl . (<$ ctx) . h)
+    (R other) -> alg hdl (R other) ctx
 
 runCatch ::
      EmitErrorsC e m a
-  -> m (Either e a)
+  -> ExceptT e m a
 runCatch =
-  runError . runEmitErrorsC
+  ExceptT . runError . runEmitErrorsC
